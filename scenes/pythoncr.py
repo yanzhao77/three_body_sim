c54137c1c2f0c1ee2b8a87ac75bd9f2b42e35e1f
march3 <13505732@qq.com>
2023-02-09 12:23:42 +0800

    三体运行模拟器

---
+++ scenes/pythoncr.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# title           :
+# description     :
+# author          :Python超人
+# date            :2023-01-22
+# notes           :
+# python_version  :3.8
+# ==============================================================================
+from bodies import Sun, Earth
+from common.consts import SECONDS_PER_WEEK
+from scenes.func import mayavi_run
+
+if __name__ == '__main__':
+    """
+    太阳、地球
+    """
+    bodies = [
+        Sun(size_scale=1.2e2, texture='douyin.jpg'),  # 太阳放大 120 倍
+        Earth(size_scale=4e3, texture='pythoncr.jpg', distance_scale=1),  # 地球放大 4000 倍，距离保持不变
+    ]
+    mayavi_run(bodies, SECONDS_PER_WEEK, view_azimuth=-45)
